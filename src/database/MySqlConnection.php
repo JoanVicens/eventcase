6314c334e13309322dfbe590220ec338ea233f9c
joanvicens <jwcervera@hotmail.com>
2021-07-25 22:59:01 +0200

    added connection to database

---
+++ src/database/MySqlConnection.php
@@ -0,0 +1,41 @@
+<?php
+
+class MySqlConnection
+{
+
+    private $con;
+
+    private string $database;
+    private string $user;
+    private string $password;
+
+    function __construct(string $database, string $user, string $password)
+    {
+        $this->database = $database;
+        $this->user = $user;
+        $this->password = $password;
+        $this->con = NULL;
+    }
+
+    public function connect()
+    {
+
+        try {
+            $this->con = new PDO("mysql:host=db;port=3306;dbname={$this->database}", $this->user, $this->password);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            echo $ex;
+        }
+        return $this->con != NULL;
+    }
+
+    public function getConnector()
+    {
+        return $this->con;
+    }
+
+    public function closeConnection()
+    {
+        $this->con = NULL;
+    }
+}
