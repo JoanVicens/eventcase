dd80c978af40b070613142d44194b07e58ec7e87
joanvicens <jwcervera@hotmail.com>
2021-07-25 18:03:26 +0200

    rent movie route added

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
-
 <?php
-require 'vendor/autoload.php';
+
+require_once 'vendor/autoload.php';
 
 use Pecee\SimpleRouter\SimpleRouter;
 
-SimpleRouter::get('/', function () {
-    return 'Hello world';
+# DATABASE
+$moviesDA = new InMemoryMoviesDB();
+$clientDA = new InMemoryClientDB();
+$rentalDA = new InMemoryRentalDB();
+
+# RENT MOVIES
+$costCalculator = new RentInDaysPriceCalculator(getenv('RENT_BASE_PRICE'), getenv('RENT_PER_DAY'));
+
+# HANDLERS
+$movieRenterHandler = new MovieRentHandler(getenv('RENT_DURATION_IN_DAYS'), $moviesDA, $clientDA, $rentalDA, $costCalculator);
+
+# CONTROLLERS
+$rentController = new RentController($movieRenterHandler);
+
+# ROUTES
+SimpleRouter::post('/rent/{id}', function($id) {
+    return $GLOBALS["rentController"] -> rent($id);
 });
+
+
 
 SimpleRouter::start();
